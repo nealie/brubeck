7ba970cfc5b4efa84f3f2e14da1eb33a7ed029f9
Neal Nelson <neal@nealmac.makalumedia.loc>
2013-01-31 16:56:49 +0100

    First properly working version (HTTP POST only).

--- brubeck/jsonrpc.py
+++ brubeck/jsonrpc.py
@@ -1,3 +1,6 @@
+"""
+Simple JSON-RPC implementation supporting connections using HTTP POST.
+"""
 
 import json
 import logging
@@ -6,14 +9,31 @@ import traceback
 
 from brubeck.connections import Mongrel2Connection
 from brubeck.request import Request
-from brubeck.request_handling import MessageHandler, JSONMessageHandler
+from brubeck.request_handling import MessageHandler, JSONMessageHandler, render
 
 
 class JsonRpcError(Exception):
+    """
+    Base class for all JSON-RPC exceptions.
+
+    All derived classes need to set:
+    - status: The status code to report for the error.
+    - id: The message id causing the error.
+    """
     pass
+
+    @property
+    def message(self):
+        """
+        Must be reimplemented for each derived Exception.
+        """
+        return None
 
 
 class MethodNotFoundError(JsonRpcError):
+    """
+    A message called an invalid method.
+    """
     def __init__(self, id, method):
         self.status = -4
         self.id = id
@@ -25,8 +45,12 @@ class MethodNotFoundError(JsonRpcError):
 
 
 class MalformedV1RequestError(JsonRpcError):
+    """
+    A JSON-RPC V1.x message was malformed.
+    """
     def __init__(self, message, key):
         self.status = JsonRpcHandler._DEFAULT_STATUS
+        self.id = None
         self.msg = message
         self.key = key
 
@@ -35,17 +59,10 @@ class MalformedV1RequestError(JsonRpcError):
         return "Malformed message'{0}', key '{1}' not found.".format(self.msg, self.key)
 
 
-#class JsonRpcV2Error(JsonRpcError):
-#    def __init__(self, message):
-#        self.status = JsonRpcHandler._DEFAULT_STATUS
-#        self.msg = message
-#
-#    @property
-#    def message(self):
-#        return "JSON-RPC V2.0 not supported: {0}".format(self.mesmsgsage)
-
-
 class JsonRpcExecError(JsonRpcError):
+    """
+    An error ocurred executing a method.
+    """
     def __init__(self, msgid, method, where, why, how):
         self.status = JsonRpcHandler._SERVER_ERROR
         self.id = msgid
@@ -67,20 +84,24 @@ def embed_methods(cls):
     """
     for name, method in cls.__dict__.iteritems():
         if hasattr(method, "_jsonrpc_method"):
-            # Add to the methods dictionary.
-            #cls.methods[name] = method
             cls._method_names.append(name)
     return cls
 
 def method(view):
     """
-    Decorator to mark as a JSON-RPC method.
+    Decorator to mark a method as a JSON-RPC method.
     """
     view._jsonrpc_method = True
     return view
 
+def method_func(func, cls):
+    """
+    Decorator to mark a function as a JSON-RPC method of a class.
+    """
+    cls.methods.append(func)
 
-class JsonRpcHandler(MessageHandler):
+
+class JsonRpcHandler(JSONMessageHandler):
 
     VERSION = "version"
     JSONRPC = "jsonrpc"
@@ -95,7 +116,7 @@ class JsonRpcHandler(MessageHandler):
 
     def __init__(self, *args, **kwargs):
         """
-        Generate a dict of bound methods from the method names stored at clas creation time.
+        Generate a dict of bound methods from the method names stored at class creation time.
         """
         super(JsonRpcHandler, self).__init__(*args, **kwargs)
         self.methods = {}
@@ -103,11 +124,12 @@ class JsonRpcHandler(MessageHandler):
             self.methods[name] = getattr(self, name)
 
     def __call__(self):
+        """
+        Process the message.
+        """
         try:
-            #data = self.message.data
-            data = self.message.body
-            print "Got", repr(data)
-            data = json.loads(data)
+            logging.info("Request: {0}".format(self.message.body))
+            data = json.loads(self.message.body)
             if self.JSONRPC in data:
                 # Version 2.0
                 result = self._process_v2(data)
@@ -128,9 +150,11 @@ class JsonRpcHandler(MessageHandler):
         elif self.JSONRPC in self.message.data:
             result[self.JSONRPC] = self.message.data[self.JSONRPC]
 
-        print repr(result)
-        #return json.dumps(result)
-        return result
+        logging.info("Response: {0}".format(result))
+
+        self._payload["data"] = result
+
+        return self.render(hide_status=True)
 
     def _process_v1(self, data):
         """
@@ -138,7 +162,6 @@ class JsonRpcHandler(MessageHandler):
         """
         try:
             msgid, method, params = data["id"], data["method"], data["params"]
-            print "ID", msgid, "Method", method, "Params", params
         except KeyError, error:
             raise MalformedV1RequestError(data, error.args[0])
         if method not in self.methods:
@@ -163,39 +186,3 @@ class JsonRpcHandler(MessageHandler):
         At the moment we only support messages formatted like V1.x ;-).
         """
         return self._process_v1(data)
-
-
-
-from request import to_bytes
-
-class JsonRpcConnection(Mongrel2Connection):
-    """
-    """
-
-    def process_message(self, application, message):
-        print "process_message", message
-        request = Request.parse_msg(message)
-        print request
-        if request.is_disconnect():
-            return
-        handler = application.route_message(request)
-        response = handler()
-
-        print "Response:", repr(response)
-        application.msg_conn.reply(request, json.dumps(response))
-
-    def send(self, uuid, conn_id, msg):
-        """Raw send to the given connection ID at the given uuid, mostly used
-        internally.
-        """
-        header = "%s %d:%s," % (uuid, len(str(conn_id)), str(conn_id))
-        print "Send:", uuid, conn_id, repr(msg)
-        print repr(header + ' ' + to_bytes(msg))
-        self.out_sock.send(header + ' ' + to_bytes(msg) + "\n\n")
-        print "Sent"
-
-    def reply(self, req, msg):
-        """Does a reply based on the given Request object and message.
-        """
-        print "Reply", req, repr(msg)
-        self.send(req.sender, req.conn_id, msg)
